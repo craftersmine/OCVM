f55db53705344fae8415a20d704df8bf7a2ed846
craftersmine <rodion.morozov.01@gmail.com>
2020-06-13 20:53:24 +0700

    Added LuaCallback attribute

---
+++ craftersmine.OCVM.Core/Attributes/LuaCallbackAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.OCVM.Core.Attributes
+{
+    [System.AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public sealed class LuaCallbackAttribute : Attribute
+    {
+        public LuaCallbackAttribute()
+        {
+        }
+
+        public bool IsDirect { get; set; } = false;
+        public string Doc { get; set; } = "";
+    }
+}
