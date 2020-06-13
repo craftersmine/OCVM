ca2749f435338b7791e6a5ed2337716f2b7e0648
craftersmine <rodion.morozov.01@gmail.com>
2020-06-13 13:30:53 +0700

    Updated LuaExecutionModule to work with NLua

--- craftersmine.OCVM.Core/Base/LuaApi/LuaExecutionModule.cs
+++ craftersmine.OCVM.Core/Base/LuaApi/LuaExecutionModule.cs
@@ -4,51 +4,84 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
+using System.IO;
 using craftersmine.OCVM.Core.Base;
 using craftersmine.OCVM.Core.Base.LuaApi.OpenComputers;
-using MoonSharp.Interpreter;
-using MoonSharp.Interpreter.Loaders;
-using System.IO;
-using MoonSharp.VsCodeDebugger;
+using NLua;
 
 namespace craftersmine.OCVM.Core.Base.LuaApi
 {
     public sealed class LuaExecutionModule
     {
         //private int maxRamForLoad;
-        private readonly Script env;
-        private MoonSharpVsCodeDebugServer svc;
+        //private readonly Script env;
+        private readonly Lua env;
 
         public LuaExecutionModule(int maxRamAvailable)
         {
-            env = new Script();
-            svc = new MoonSharpVsCodeDebugServer();
+            env = new Lua();
+            env.LoadCLRPackage();
             RegisterGlobals();
             RegisterModules();
-            svc.Start();
-            svc.AttachToScript(env, "mainChunk");
         }
 
-        public async void ExecuteFile(string path)
+        private void PrintException(Exception e)
         {
-            env.Options.ScriptLoader = new FileSystemScriptLoader() { IgnoreLuaPathGlobal = true, ModulePaths = new string[] { Path.GetDirectoryName(path) } };
+            VM.RunningVM.Display.SetColor(BaseColors.Black, BaseColors.Red);
+            Root.print(e.Source + e.Message);
+            VM.RunningVM.Display.SetColor(BaseColors.Black, BaseColors.White);
+        }
+
+        public async Task<object[]> ExecuteFile(string path)
+        {
+            //env.Options.ScriptLoader = new FileSystemScriptLoader() { IgnoreLuaPathGlobal = true, ModulePaths = new string[] { Path.GetDirectoryName(path) } };
             //await env.DoFileAsync(path);
+            return await Task<object[]>.Run(new Func<object[]>(() => {
+                try
+                {
+                    return env.DoFile(path);
+                }
+                catch (Exception ex)
+                {
+                    PrintException(ex);
+                    return null;
+                }
+            }));
         }
 
-        public void ExecuteString(string str, string chunkName = "mainChunk")
+        public async Task<object[]> ExecuteString(string str, string chunkName = "mainChunk")
         {
-            env.DoString(str, env.Globals, chunkName);
-            //svc.Detach(env);
+            return await Task<object[]>.Run(new Func<object[]>(() => {
+                try
+                {
+                    return env.DoString(str, chunkName);
+                }
+                catch (Exception ex)
+                {
+                    PrintException(ex);
+                    return null;
+                }
+            }));
         }
 
         public void RegisterGlobals()
         {
-            env.Globals["print"] = (Action<string>)Root.print;
+            //env.Globals["print"] = (Action<string>)Root.print;
+            env.RegisterFunction("print", typeof(Root).GetMethod("print"));
+            env.RegisterFunction("breakpoint", typeof(Root).GetMethod("breakpoint"));
         }
 
         public void RegisterModules()
         {
-            env.Globals.RegisterModuleType<Component>();
+            env["component"] = new Component();
+        }
+
+        public LuaTable CreateTable()
+        {
+            env.NewTable("tempTable");
+            LuaTable table = env["tempTable"] as LuaTable;
+            env["tempTable"] = null;
+            return table;
         }
     }
 }
