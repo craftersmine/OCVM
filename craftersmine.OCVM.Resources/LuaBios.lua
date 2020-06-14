b34082134a3a181ebcbbe9fe5a9802700252325d
craftersmine <rodion.morozov.01@gmail.com>
2020-06-14 14:14:43 +0700

    Test BIOS update

--- craftersmine.OCVM.Resources/LuaBios.lua
+++ craftersmine.OCVM.Resources/LuaBios.lua
@@ -1,8 +1,8 @@
 local component = require('component');
 
-print(component.type('0011'))
-print(component.slot('0011'))
+--print(component.type('0011'))
+--print(component.slot('0011'))
 p = component.proxy(component.list("eeprom")());
 for k, v in pairs(p) do
-	print(k,v)
+	print(k, v);
 end
