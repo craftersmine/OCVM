0298e651978bf75777acfe023d7dafa5d5c2d6cd
craftersmine <rodion.morozov.01@gmail.com>
2020-06-14 16:17:18 +0700

    Added class for computer component API

---
+++ craftersmine.OCVM.Core/Base/LuaApi/OpenComputers/Computer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using craftersmine.OCVM.Core.MachineComponents;
+using craftersmine.OCVM.Core.Extensions;
+using NLua;
+
+namespace craftersmine.OCVM.Core.Base.LuaApi.OpenComputers
+{
+    class Computer
+    {
+    }
+}
