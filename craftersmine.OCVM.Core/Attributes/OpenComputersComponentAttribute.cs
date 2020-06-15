576a92b0356f4c9485a283381b198c156a4b9dc8
craftersmine <rodion.morozov.01@gmail.com>
2020-06-15 21:28:57 +0700

    Added OpenComputersComponentAttribute

---
+++ craftersmine.OCVM.Core/Attributes/OpenComputersComponentAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.OCVM.Core.Attributes
+{
+    [System.AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class OpenComputersComponentAttribute : Attribute
+    {
+        public OpenComputersComponentAttribute()
+        {
+        }
+
+        public string ComponentType { get; set; } = "generic";
+    }
+}
