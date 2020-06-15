b9261ec7911b6bca82f99418d7b6eb43a70cec3b
craftersmine <rodion.morozov.01@gmail.com>
2020-06-15 21:37:13 +0700

    Added computer.lua

---
+++ craftersmine.OCVM.GUI/lua/computer.lua
@@ -0,0 +1,13 @@
+﻿import('craftersmine.OCVM.Core', 'craftersmine.OCVM.Core.Base.LuaApi.OpenComputers');
+
+local computer = {}
+
+function computer.address()
+	return Computer.address();
+end
+
+function computer.getBootAddress()
+	return Computer.getBootAddress();
+end
+
+return computer;
