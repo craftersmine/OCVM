fd945e864730b4698ecdc969bc2256933a141071
craftersmine <rodion.morozov.01@gmail.com>
2020-06-15 21:33:16 +0700

    Added Computer OC component

--- craftersmine.OCVM.Core/MachineComponents/Computer.cs
+++ craftersmine.OCVM.Core/MachineComponents/Computer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using craftersmine.OCVM.Core.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,12 @@ using System.Threading.Tasks;
 
 namespace craftersmine.OCVM.Core.MachineComponents
 {
-    public sealed class ComputerComponent : BaseComponent
+    [OpenComputersComponent(ComponentType = "computer")]
+    public sealed class Computer : BaseComponent
     {
-
+        public Computer(string address) : base(address)
+        { 
+            
+        }
     }
 }
