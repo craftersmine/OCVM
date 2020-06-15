8864cd471de8b143cb4132a593ab61664b322072
craftersmine <rodion.morozov.01@gmail.com>
2020-06-15 21:29:24 +0700

    Added ObjectExtensions

---
+++ craftersmine.OCVM.Core/Extensions/ObjectExtensions.cs
@@ -0,0 +1,20 @@
+﻿using craftersmine.OCVM.Core.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.OCVM.Core.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static MethodInfo[] GetLuaCallbacks(this object obj)
+        {
+            Type objType = obj.GetType();
+            var reflectedMethods = objType.GetMethods().Where(m => m.GetCustomAttributes(typeof(LuaCallbackAttribute), false).Length > 0).ToArray();
+            return reflectedMethods;
+        }
+    }
+}
