b2150f25bc4eede7b81ee3653e276b765bf5f5bf
craftersmine <rodion.morozov.01@gmail.com>
2020-06-15 21:29:11 +0700

    Added InvalidDeviceException

---
+++ craftersmine.OCVM.Core/Exceptions/InvalidDeviceException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.OCVM.Core.Exceptions
+{
+
+    [Serializable]
+    public class InvalidDeviceException : Exception
+    {
+        public InvalidDeviceException() { }
+        public InvalidDeviceException(string message) : base(message) { }
+        public InvalidDeviceException(string message, Exception inner) : base(message, inner) { }
+        protected InvalidDeviceException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
