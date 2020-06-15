80ddca2ab4cb8a774d632f7cd1011fc1b8613506
craftersmine <rodion.morozov.01@gmail.com>
2020-06-15 21:32:19 +0700

    Updated Computer LuaAPI class

--- craftersmine.OCVM.Core/Base/LuaApi/OpenComputers/Computer.cs
+++ craftersmine.OCVM.Core/Base/LuaApi/OpenComputers/Computer.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 using craftersmine.OCVM.Core.MachineComponents;
 using craftersmine.OCVM.Core.Extensions;
 using NLua;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace craftersmine.OCVM.Core.Base.LuaApi.OpenComputers
 {
-    class Computer
+    public class Computer
     {
+        public static string address()
+        {
+            var dev = VM.RunningVM.DeviceBus.GetPrimaryComponent("computer");
+            return dev.Address;
+        }
+
+        public static string tmpAddress()
+        {
+            return Guid.Empty.ToString();
+        }
+        
+        public static void beep(int freq, float duration)
+        { }
+
+        public static string getBootAddress()
+        {
+            var primaryFs = VM.RunningVM.DeviceBus.GetPrimaryComponent("filesystem");
+            if (primaryFs != null)
+                return primaryFs.Address;
+            else return null;
+        }
     }
 }
