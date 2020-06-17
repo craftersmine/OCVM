9bc8afcce4003a907a519098d25e48fa76c30ad7
craftersmine <rodion.morozov.01@gmail.com>
2020-06-17 22:15:05 +0700

    Added few methods wrappers to computer.lua

--- craftersmine.OCVM.GUI/lua/computer.lua
+++ craftersmine.OCVM.GUI/lua/computer.lua
@@ -10,9 +10,31 @@ function computer.getBootAddress()
 	return Computer.getBootAddress();
 end
 
-function computer.beep( ... )
-	
+function computer.setBootAddress(address)
+	if not address then address = "";
+	Computer.setBootAddress(address);
 end
 
+function computer.beep(frequency, duration)
+	if not frequency then frequency = 440 end;
+	if not duration then duration = 0.1 end;
+	Computer.beep(frequency, duration);
+end
+
+function computer.uptime()
+	return Computer.uptime();
+end
+
+function computer.tmpAddress()
+	return "";
+end
+
+function computer.getFreeMemory()
+	return Computer.getFreeMemory();
+end
+
+function computer.getTotalMemory()
+	return Computer.getTotalMemory();
+end
 
 return computer;
