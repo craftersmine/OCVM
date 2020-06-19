e072ba0a647497f839d8782e502da4afa13d2811
craftersmine <rodion.morozov.01@gmail.com>
2020-06-20 00:53:48 +0700

    Made Computer component functions

--- craftersmine.OCVM.Core/MachineComponents/Computer.cs
+++ craftersmine.OCVM.Core/MachineComponents/Computer.cs
@@ -1,4 +1,6 @@
 ﻿using craftersmine.OCVM.Core.Attributes;
+using craftersmine.OCVM.Core.Base.LuaApi;
+using NLua;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +12,13 @@ namespace craftersmine.OCVM.Core.MachineComponents
     [OpenComputersComponent(ComponentType = "computer")]
     public sealed class Computer : BaseComponent
     {
+        private List<string> users = new List<string>();
+        private Queue<Signal> signalQueue = new Queue<Signal>();
+
+        public int AllocatedVirtualMemory { get; set; }
+        public int FreeMemory { get { return AllocatedVirtualMemory / 2; } }
+        public string PrimaryBootDevice { get; set; }
+
         public Computer(string address) : base(address)
         {
             DeviceInfo.Class = DeviceClass.Processor;
@@ -17,6 +26,58 @@ namespace craftersmine.OCVM.Core.MachineComponents
             DeviceInfo.Product = "craftersmine OpenComputers Virtual Machine";
             DeviceInfo.Vendor = "craftersmine";
             DeviceInfo.Version = VM.CurrentVersion.ToString();
+        }
+
+        public bool AddUser(string user, out string errorMsg)
+        {
+            if (users.Contains(user))
+            {
+                errorMsg = OCErrors.UserExists;
+                return false;
+            }
+            else
+            {
+                users.Add(user);
+                errorMsg = null;
+                return true;
+            }
+        }
+
+        public bool RemoveUser(string user)
+        {
+            if (users.Contains(user))
+            {
+                users.Remove(user);
+                return true;
+            }
+            else return false;
+        }
+        
+        public string[] GetUsers()
+        {
+            return users.ToArray();
+        }
+
+        public void PushSignal(string name, LuaTable data)
+        {
+            signalQueue.Enqueue(new Signal(name, data));
+        }
+
+        public Signal PullSignal()
+        {
+            return signalQueue.Dequeue();
+        }
+    }
+
+    public sealed class Signal
+    {
+        public string Name { get; set; }
+        public LuaTable Data { get; set; }
+
+        private Signal() { }
+        public Signal(string name, LuaTable data)
+        {
+            Name = name; Data = data;
         }
     }
 }
