246d24cc0a710ce5d5884b223aeed825bb80ccc6
craftersmine <rodion.morozov.01@gmail.com>
2020-06-20 00:42:42 +0700

    Moved computer.lua to Resources folder

--- craftersmine.OCVM.Resources/lua/computer.lua
+++ craftersmine.OCVM.Resources/lua/computer.lua
@@ -41,4 +41,29 @@ function computer.getDeviceInfo()
 	return Computer.getDeviceInfo();
 end
 
+function computer.users()
+	return table.unpack(Computer.users());
+end
+
+function computer.addUser(user)
+	return Computer.addUser(user);
+end
+
+function computer.removeUser(user)
+	return Computer.removeUser(user);
+end
+
+function computer.pushSignal(name, ...)
+	Computer.pushSignal(name, {...});
+end
+
+function computer.pullSignal(timeout)
+	if not timeout and type(timeout) ~= "number" then timeout = 0 end;
+	name, data = Computer.pullSignal(timeout);
+end
+
+function computer.getProgramLocations()
+	return {};
+end
+
 return computer;
