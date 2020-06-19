ccb4b0688a2f232554bef1d30d924fa686e45844
craftersmine <rodion.morozov.01@gmail.com>
2020-06-20 00:58:46 +0700

    Added checkArg function

--- craftersmine.OCVM.Resources/lua/stdlib.lua
+++ craftersmine.OCVM.Resources/lua/stdlib.lua
@@ -1,2 +1,16 @@
 -- Contains overrides to specific Lua functions
 
+local std = {}
+
+function std.checkArg(n, have, ...)
+    have = type(have)
+    result = checkArgType(have, ...);
+    if not result then
+        local msg = string.format("bad argument #%d (%s expected, got %s)", n, table.concat({...}, " or "), have)
+        breakpoint(msg);
+        error(msg, 3)
+    else return;
+    end
+end
+
+return std;
