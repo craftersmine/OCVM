6941da7971334baf7f6c4e286178bb820a93f3cd
craftersmine <rodion.morozov.01@gmail.com>
2020-06-20 00:56:57 +0700

    Added VMReady and VMStateChanged events

--- craftersmine.OCVM.Core/VMEvents.cs
+++ craftersmine.OCVM.Core/VMEvents.cs
@@ -9,10 +9,22 @@ namespace craftersmine.OCVM.Core
     public sealed class VMEvents
     {
         public static event EventHandler<DiskActivityEventArgs> DiskActivity;
+        public static event EventHandler VMReady;
+        public static event EventHandler<VMStateChangedEventArgs> VMStateChanged;
+
+        public static void OnVMReady()
+        {
+            VMReady?.Invoke(VM.RunningVM, EventArgs.Empty);
+        }
+
+        public static void OnVMStateChanged(VMState state)
+        {
+            VMStateChanged?.Invoke(VM.RunningVM, new VMStateChangedEventArgs() { State = state });
+        }
 
         public static void OnDiskActivity(string fsAddress, DiskActivityType activityType)
         {
-            DiskActivity?.Invoke(null, new DiskActivityEventArgs() { FileSystemAddress = fsAddress, DiskActivityType = activityType });
+            DiskActivity?.Invoke(VM.RunningVM, new DiskActivityEventArgs() { FileSystemAddress = fsAddress, DiskActivityType = activityType });
         }
     }
 
@@ -20,6 +32,11 @@ namespace craftersmine.OCVM.Core
     {
         public DiskActivityType DiskActivityType { get; set; }
         public string FileSystemAddress { get; set; }
+    }
+
+    public sealed class VMStateChangedEventArgs : EventArgs
+    {
+        public VMState State { get; set; }
     }
 
     public enum DiskActivityType
