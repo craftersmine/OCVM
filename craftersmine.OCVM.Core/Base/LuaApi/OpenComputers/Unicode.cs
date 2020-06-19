caf8ab73ba196f3b20bf1feaa072fa49311d433c
craftersmine <rodion.morozov.01@gmail.com>
2020-06-20 00:47:49 +0700

    Added Unicode.cs

---
+++ craftersmine.OCVM.Core/Base/LuaApi/OpenComputers/Unicode.cs
@@ -0,0 +1,39 @@
+﻿using craftersmine.OCVM.Core.Extensions;
+using NLua;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.OCVM.Core.Base.LuaApi.OpenComputers
+{
+    public sealed class Unicode
+    {
+        public static LuaTable _char(LuaTable args)
+        {
+            LuaTable chars = VM.RunningVM.ExecModule.CreateTable();
+            var _args = args.GetValuesAsArray();
+            List<byte> bytes = new List<byte>();
+            for (int i = 0; i < _args.Length; i++)
+            {
+                    bytes.Add(Convert.ToByte(_args[i]));
+            }
+
+            char[] chrs = Encoding.UTF8.GetChars(bytes.ToArray());
+
+            for (int i = 1; i <= chrs.Length; i++)
+            {
+                chars[i] = chrs[i - 1].ToString();
+            }
+
+            return chars;
+        }
+
+        public static int charWidth(string _char)
+        {
+            char chr = _char[0];
+            return Encoding.UTF8.GetByteCount(chr.ToString());
+        }
+    }
+}
