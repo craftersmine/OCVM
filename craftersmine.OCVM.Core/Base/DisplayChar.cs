2e342b805bb8d620bd7a40f5f2ec3cf498f58293
craftersmine <rodion.morozov.01@gmail.com>
2020-06-21 15:53:29 +0700

    Updated DisplayChar. IsInvalidated property not used, but may be useful

--- craftersmine.OCVM.Core/Base/DisplayChar.cs
+++ craftersmine.OCVM.Core/Base/DisplayChar.cs
@@ -12,12 +12,43 @@ namespace craftersmine.OCVM.Core.Base
         public char Character { get; set; }
         public Color ForegroundColor { get; set; }
         public Color BackgroundColor { get; set; }
+        public bool IsInvalidated { get; set; }
 
         public DisplayChar(char chr, Color foreground, Color background)
         {
             Character = chr;
             ForegroundColor = foreground;
             BackgroundColor = background;
+            IsInvalidated = true;
+        }
+
+        public void SetChar(char chr, Color foreground, Color background)
+        {
+            Character = chr;
+            ForegroundColor = foreground;
+            BackgroundColor = background;
+            IsInvalidated = true;
+        }
+
+        public void SetChar(char chr)
+        {
+            SetChar(chr, ForegroundColor, BackgroundColor);
+        }
+
+        public bool CheckInvalidation(char chr, Color foreground, Color background)
+        {
+            if (Character != chr)
+                return true;
+            if (ForegroundColor != foreground)
+                return true;
+            if (BackgroundColor != background)
+                return true;
+            else return false;
+        }
+
+        public override string ToString()
+        {
+            return Character.ToString();
         }
     }
 }
