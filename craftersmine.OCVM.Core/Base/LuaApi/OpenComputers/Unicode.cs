f72db15fe45fcbb79f70b87fa5d885a57e5ee763
craftersmine <rodion.morozov.01@gmail.com>
2020-06-26 00:22:33 +0700

    Completed Unicode API

--- craftersmine.OCVM.Core/Base/LuaApi/OpenComputers/Unicode.cs
+++ craftersmine.OCVM.Core/Base/LuaApi/OpenComputers/Unicode.cs
@@ -14,13 +14,12 @@ namespace craftersmine.OCVM.Core.Base.LuaApi.OpenComputers
         {
             LuaTable chars = VM.RunningVM.ExecModule.CreateTable();
             var _args = args.GetValuesAsArray();
-            List<byte> bytes = new List<byte>();
+            List<int> values = new List<int>();
+            char[] chrs = new char[_args.Length];
             for (int i = 0; i < _args.Length; i++)
             {
-                    bytes.Add(Convert.ToByte(_args[i]));
+                chrs[i] = (char)Convert.ToInt32(_args[i]);
             }
-
-            char[] chrs = Encoding.UTF8.GetChars(bytes.ToArray());
 
             for (int i = 1; i <= chrs.Length; i++)
             {
@@ -46,6 +45,54 @@ namespace craftersmine.OCVM.Core.Base.LuaApi.OpenComputers
         public static int len(string str)
         {
             return Encoding.UTF8.GetCharCount(Encoding.UTF8.GetBytes(str));
+        }
+
+        public static string lower(string str)
+        {
+            return str.ToLower();
+        }
+
+        public static string reverse(string str)
+        {
+            return new string(str.Reverse().ToArray());
+        }
+
+        public static string sub(string str, int start)
+        {
+            start -= 1;
+            return str.Substring(start);
+        }
+
+        public static string sub(string str, int start, int end)
+        {
+            start -= 1;
+            return str.Substring(start, end);
+        }
+
+        public static string upper(string str)
+        {
+            return str.ToUpper();
+        }
+
+        public static int wlen(string str)
+        {
+            return Encoding.UTF8.GetBytes(str).Length;
+        }
+
+        public static string wtrunc(string str, int count)
+        {
+            var bytes = Encoding.UTF8.GetBytes(str);
+            byte[] truncated = new byte[count];
+            if (bytes.Length < count)
+                return "";
+            else
+            {
+                for (int i = 0; i <= count; i++)
+                {
+                    truncated[i] = bytes[i];
+                }
+                return Encoding.UTF8.GetString(truncated);
+            }
         }
     }
 }
