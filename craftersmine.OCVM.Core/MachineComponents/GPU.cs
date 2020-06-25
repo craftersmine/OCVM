e8a7511f8083589a6c19738a135f7f67888278e3
craftersmine <rodion.morozov.01@gmail.com>
2020-06-26 00:19:11 +0700

    Added GPU and Screen component

---
+++ craftersmine.OCVM.Core/MachineComponents/GPU.cs
@@ -0,0 +1,31 @@
+﻿using craftersmine.OCVM.Core.Attributes;
+using craftersmine.OCVM.Core.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.OCVM.Core.MachineComponents
+{
+    [OpenComputersComponent(ComponentType = "gpu")]
+    public sealed class GPU : BaseComponent
+    {
+        public GPU() : base()
+        {
+            
+        }
+
+        public object[] get(int x, int y)
+        {
+            var data = new object[5];
+            var v = ScreenBufferManager.Instance.GetBuffer(0).Get(x, y);
+            data[0] = v.Character.ToString();
+            data[1] = v.ForegroundColor.ToArgb();
+            data[2] = v.BackgroundColor.ToArgb();
+            data[3] = null;
+            data[4] = null;
+            return data;
+        }
+    }
+}
---
+++ craftersmine.OCVM.Core/MachineComponents/Screen.cs
@@ -0,0 +1,98 @@
+﻿using craftersmine.OCVM.Core.Attributes;
+using craftersmine.OCVM.Core.Base;
+using NLua;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.OCVM.Core.MachineComponents
+{
+    [OpenComputersComponent(ComponentType = "screen")]
+    public sealed class Screen : BaseComponent
+    {
+        internal bool IsOn { get; set; }
+        internal bool IsPresiseModeEnabled { get; set; }
+        internal bool IsTouchModeInverted { get; set; }
+
+        public Screen(Tier tier) : base()
+        {
+
+        }
+
+        [LuaCallback(IsDirect = true)]
+        public bool isOn()
+        {
+            return IsOn;
+        }
+
+        [LuaCallback(IsDirect = false)]
+        public bool turnOn()
+        {
+            IsOn = true;
+            VMEvents.OnScreenStateChanged(this);
+            return IsOn;
+        }
+
+        [LuaCallback(IsDirect = false)]
+        public bool turnOff()
+        {
+            IsOn = false;
+            VMEvents.OnScreenStateChanged(this);
+            return IsOn;
+        }
+
+        [LuaCallback(IsDirect = true)]
+        public object[] getAspectRatio()
+        {
+            var buff = ScreenBufferManager.Instance.GetBuffer(0);
+            if (buff != null)
+                return new object[] { buff.Width, buff.Height };
+            else return new object[] { 1, 1 };
+        }
+
+        [LuaCallback(IsDirect = false)]
+        public LuaTable getKeyboards()
+        {
+            LuaTable table = VM.RunningVM.ExecModule.CreateTable();
+            var kbds = VM.RunningVM.DeviceBus.GetDevicesByType("keyboard", false);
+            for (int i = 1; i <= kbds.Length; i++)
+            {
+                table[i] = kbds[i - 1];
+            }
+            return table;
+        }
+
+        [LuaCallback(IsDirect = true)]
+        public bool isPresise()
+        {
+            return IsPresiseModeEnabled;
+        }
+
+        [LuaCallback(IsDirect = false)]
+        public bool setPresise(bool enabled)
+        {
+            bool old = IsPresiseModeEnabled;
+            IsPresiseModeEnabled = enabled;
+            return old;
+        }
+
+        [LuaCallback(IsDirect = true)]
+        public bool isTouchModeInverted()
+        {
+            return IsTouchModeInverted;
+        }
+
+        public bool setTouchModeInverted(bool value)
+        {
+            bool old = IsTouchModeInverted;
+            if (old != value)
+            {
+                IsTouchModeInverted = value;
+                VMEvents.OnScreenStateChanged(this);
+            }
+            return old;
+        }
+    }
+}
