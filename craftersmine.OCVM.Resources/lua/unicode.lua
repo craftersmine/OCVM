bc4f4628212cbb8f889fd85789b1f43f6c2279e2
craftersmine <rodion.morozov.01@gmail.com>
2020-06-26 00:13:45 +0700

    Completed Unicode API

--- craftersmine.OCVM.Resources/lua/unicode.lua
+++ craftersmine.OCVM.Resources/lua/unicode.lua
@@ -2,17 +2,13 @@ import('craftersmine.OCVM.Core', 'craftersmine.OCVM.Core.Base.LuaApi.OpenCompute
 
 local unicode = {}
 
-function unicode.char(value, ...)
-	if type(value) == "string" and value ~= nil then
-		data = Unicode._char({...})
-		return table.unpack(data);
-	else
-		return nil;
-	end;
+function unicode.char(...)
+	data = Unicode._char({...})
+	return table.unpack(data);
 end;
 
 function unicode.charWidth(value, ...)
-	return Unicode.charWidth(tostring(value))
+	return Unicode.charWidth(value)
 end
 
 function unicode.isWide(value, ...)
@@ -21,6 +17,32 @@ end
 
 function unicode.len(value)
 	return Unicode.len(tostring(value));
+end
+
+function unicode.lower(value)
+	return Unicode.lower(tostring(value));
+end
+
+function unicode.reverse(value)
+	return Unicode.reverse(value);
+end
+
+function unicode.sub(str, start, endV)
+	if endV == nil or endV == 0 then return Unicode.sub(str, start)
+	else return Unicode.sub(str, start, endV);
+	end;
+end
+
+function unicode.upper(str)
+	return Unicode.upper(str);
+end
+
+function unicode.wlen(str)
+	return Unicode.wlen(str);
+end
+
+function unicode.wtrunc(str, count)
+	return Unicode.wtrunc(str, count);
 end
 
 return unicode;
