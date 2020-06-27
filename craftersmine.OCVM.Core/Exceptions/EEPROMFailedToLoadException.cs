24e3640673f0634f8f74d8e4d8ec53e577ed8a9d
craftersmine <rodion.morozov.01@gmail.com>
2020-06-28 02:36:34 +0700

    Added EEPROMFailedToLoad Exception

---
+++ craftersmine.OCVM.Core/Exceptions/EEPROMFailedToLoadException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.OCVM.Core.Exceptions
+{
+
+    [Serializable]
+    public class EEPROMFailedToLoadException : Exception
+    {
+        public EEPROMFailedToLoadException() { }
+        public EEPROMFailedToLoadException(string message) : base(message) { }
+        public EEPROMFailedToLoadException(string message, Exception inner) : base(message, inner) { }
+        protected EEPROMFailedToLoadException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
