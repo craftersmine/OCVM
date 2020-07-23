0ab5d15a89c5a9d070864863523db8acd5e254f5
craftersmine <rodion.morozov.01@gmail.com>
2020-07-23 19:26:49 +0700

    Added Settings

---
+++ craftersmine.OCVM.Core/Settings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace craftersmine.OCVM.Core
+{
+    public static class Settings
+    {
+        private static bool enableLuaLogging = false; 
+        private static PrivateFontCollection pfc = new PrivateFontCollection();
+
+        public static bool CapScreenDepth { get; set; }
+        public static bool EnableLuaLogging { get { return enableLuaLogging & EnableLogging; } set { enableLuaLogging = value; } }
+        public static bool EnableLogging { get; set; }
+
+        public static Font DisplayFont { get; private set; }
+        public static int GpuMaxWidth { get; set; } = 160;
+        public static int GpuMaxHeight { get; set; } = 50;
+
+        public static void InitializeFont()
+        {
+            Logger.Instance.Log(LogEntryType.Info, "Initializing Unscii display font...");
+            pfc.AddFontFile(Path.Combine(Application.StartupPath, "unscii-16-full.ttf"));
+            DisplayFont = new Font(pfc.Families[0], 16f, FontStyle.Regular, GraphicsUnit.Pixel);
+            Logger.Instance.Log(LogEntryType.Info, "Display font successfully initialized!");
+        }
+    }
+}
