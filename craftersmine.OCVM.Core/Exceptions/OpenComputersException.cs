6d478c99c080aba448aff26022a802949fa37869
craftersmine <rodion.morozov.01@gmail.com>
2020-07-23 19:28:01 +0700

    Added OpenComputersException

---
+++ craftersmine.OCVM.Core/Exceptions/OpenComputersException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.OCVM.Core.Exceptions
+{
+    [Serializable]
+    public class OpenComputersException : Exception
+    {
+        public OpenComputersException() { }
+        public OpenComputersException(string message) : base(message) { }
+        public OpenComputersException(string message, Exception inner) : base(message, inner) { }
+        protected OpenComputersException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
