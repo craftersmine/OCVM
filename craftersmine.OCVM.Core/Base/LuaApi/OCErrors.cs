2a2c9df8346a8791afa422074717f87c3dfc182f
craftersmine <rodion.morozov.01@gmail.com>
2020-07-29 21:19:31 +0700

    Changed type of fields from static readonly to const and added some errors

--- craftersmine.OCVM.Core/Base/LuaApi/OCErrors.cs
+++ craftersmine.OCVM.Core/Base/LuaApi/OCErrors.cs
@@ -8,18 +8,20 @@ namespace craftersmine.OCVM.Core.Base.LuaApi
 {
     public static class OCErrors
     {
-        public static readonly string NoSuchComponent = "no such component";
-        public static readonly string FileNotFound = "file not found";
-        public static readonly string BadFileDescriptor = "bad file descriptor";
-        public static readonly string UnsupportedMode = "unsupported mode";
-        public static readonly string PermissionDenied = "access denied";
-        public static readonly string UserExists = "user exists";
-        public static readonly string NoBootableMediumFound = "no bootable medium found";
-        public static readonly string MachineHalted = "computer halted";
-        public static readonly string InvalidFillValue = "invalid fill value";
-        public static readonly string InvalidAddress = "invalid address";
-        public static readonly string NotAScreen = "not a screen";
-        public static readonly string InvalidPaletteIndex = "invalid palette index";
-        public static readonly string UnsupportedDepth = "unsupported depth";
+        public const string NoSuchComponent = "no such component";
+        public const string FileNotFound = "file not found";
+        public const string BadFileDescriptor = "bad file descriptor";
+        public const string UnsupportedMode = "unsupported mode";
+        public const string PermissionDenied = "access denied";
+        public const string UserExists = "user exists";
+        public const string NoBootableMediumFound = "no bootable medium found";
+        public const string MachineHalted = "computer halted";
+        public const string InvalidFillValue = "invalid fill value";
+        public const string InvalidAddress = "invalid address";
+        public const string NotAScreen = "not a screen";
+        public const string InvalidPaletteIndex = "invalid palette index";
+        public const string UnsupportedDepth = "unsupported depth";
+        public const string InvalidBufferIndex = "invalid buffer index";
+        public const string UnsupportedViewportSize = "unsupported viewport size";
     }
 }
