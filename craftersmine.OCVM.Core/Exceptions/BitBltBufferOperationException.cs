3f978960d2e5e954fa2dc498b4d32c6d0a0760aa
craftersmine <rodion.morozov.01@gmail.com>
2020-07-29 21:23:12 +0700

    Added BitBltBufferOperationException

---
+++ craftersmine.OCVM.Core/Exceptions/BitBltBufferOperationException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.OCVM.Core.Exceptions
+{
+
+    [Serializable]
+    public class BitBltBufferOperationException : Exception
+    {
+        public BitBltBufferOperationException() { }
+        public BitBltBufferOperationException(string message) : base(message) { }
+        public BitBltBufferOperationException(string message, Exception inner) : base(message, inner) { }
+        protected BitBltBufferOperationException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
